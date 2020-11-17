2a69b2f589b1246d28bfc69920097bbb8c3fb6c0
RandyQin <randyqin@haizol.com>
2020-11-17 16:26:51 +0800

    ccc

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -5,8 +5,8 @@ const Controller = require('egg').Controller
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg'
+    ctx.body = 'hi, egg';
   }
 }
 
-module.exports = HomeController
+module.exports = HomeController;
