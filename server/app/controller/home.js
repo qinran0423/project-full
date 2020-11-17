f9051cd51d1ae0fd29d5a2fa3a926a7a0849e632
RandyQin <randyqin@haizol.com>
2020-11-17 17:16:49 +0800

    feat: test pre commit

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this
+    const { ctx } = this;
     ctx.body = 'hi, egg'
   }
 }
